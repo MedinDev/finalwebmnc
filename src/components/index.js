eafc696af7f282241b93376469c4a4a5f8bc4ccd
Medtex <medtexprog@gmail.com>
2024-03-10 07:08:59 +0800

    production 06

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export {default as Footer} from './main/Footer'
 export {default as Header} from './main/Header'
+export {default as Moq} from './self/Moq'
+export {default as Fact} from './self/Fact'
+export {default as Pay} from './self/Pay'
+export {default as Exchange} from './self/Exchange'
+
